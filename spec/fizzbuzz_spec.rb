3d08aefbbde15437a9186367f6ff132e4fbc7353
Tatiana Nantes <tatiananantes@gmail.com>
2021-10-11 17:45:01 +0100

    remove notes from spec file

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -20,25 +20,3 @@ describe 'fizzbuzz' do # just a description
     expect(fizzbuzz(18)).to eq 'fizz'
   end
 end
-
-
-# "1"
-# "2"
-# "Fizz"
-# "4"
-# "Buzz"
-# "Fizz"
-# "7"
-# "8"
-# "Fizz"
-# "Buzz"
-# "11"
-# "Fizz"
-# "13"
-# "14"
-# "FizzBuzz"
-# "16"
-# "17"
-# "Fizz"
-# "19"
-# "Buzz"
