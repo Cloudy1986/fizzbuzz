02bf94cf3e399fdb6bb00f35f6de911bbbcef7c6
Mark McLeod <markmcleod45@gmail.com>
2021-10-12 11:00:59 +0100

    Removed and added tests, and updated fizzbuzz.rb

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,16 +7,10 @@ describe 'fizzbuzz' do # just a description
   it 'returns "fizz" when passed 6' do
     expect(fizzbuzz(6)).to eq 'fizz'
   end
-  it 'returns "fizz" when passed 9' do
-    expect(fizzbuzz(9)).to eq 'fizz'
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
   end
-  it 'returns "fizz" when passed 12' do
-    expect(fizzbuzz(12)).to eq 'fizz'
-  end
-  it 'returns "fizz" when passed 15' do #remove later for fizzbuzz
-    expect(fizzbuzz(15)).to eq 'fizz'
-  end
-  it 'returns "fizz" when passed 18' do
-    expect(fizzbuzz(18)).to eq 'fizz'
+  it 'returns "buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq 'buzz'
   end
 end
